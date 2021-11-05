311b88795fcf63c9ea1026b7f16d7c6e360bc76c
Tesla <43712252+0x11DFE@users.noreply.github.com>
2021-11-05 08:53:13 +0100

    offline button selection
    Added an offline button selection as well as few other changes...

---
+++ common/addon/Volume-Key-Selector/install.sh
@@ -0,0 +1,36 @@
+# External Tools
+chmod -R 0755 $MODPATH/common/addon/Volume-Key-Selector/tools
+export PATH=$MODPATH/common/addon/Volume-Key-Selector/tools/$ARCH32:$PATH
+
+selector() {
+  [ "$1" ] && local inselector_message=$1 || local inselector_message="Hello World"
+  [ "$2" ] && local vol_up=$2 || local vol_up="Yes"
+  [ "$3" ] && local vol_down=$3 || local vol_down="No"
+  [ "$4" ] && local delay=$4 || local delay=5
+  [ "$5" ] && local max_retry_count=$5 || local max_retry_count=3
+
+  ui_print "- $inselector_message"
+  ui_print "   Vol Up += $vol_up"
+  ui_print "   Vol Down += $vol_down"
+
+  retry_count=0
+  while [ $retry_count -lt $max_retry_count ]; do
+    timeout 0 keycheck
+    timeout $delay keycheck
+    local SEL=$?
+    if [ $SEL -eq 42 ]; then
+      return 0
+    elif [ $SEL -eq 41 ]; then
+      return 1
+    else
+      retry_count=$((retry_count+1))
+      retry_left=$(($max_retry_count-$retry_count+1))
+      ui_print "  Volume key not detected, Retry left: $retry_left"
+    fi
+  done
+
+  abort "  Volume key timeout..."
+}
+
+# Keep old variable from previous versions of this
+VKSEL=selector
--- customize.sh
+++ customize.sh
@@ -1,3 +1,10 @@
-# Print footer message
-ui_print "- If you cannot get Unlimited Google Photo at original quality read https://t.me/PixelProps/92"
+# Running install
+[ -f "$MODPATH/common/install-offline.sh" ] && . $MODPATH/common/install-offline.sh
+
+# Cleanup
+[ -d "$MODPATH/common" ] && rm -rf $MODPATH/common
+
+# Finish
 ui_print "- By Tesla, Telegram: @T3SL4"
+ui_print "  https://t.me/PixelProps"
+ui_print "  https://github.com/Pixel-Props"
